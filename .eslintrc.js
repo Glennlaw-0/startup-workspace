7dccfd3f07f8e78bbb7c10697fbeafc3151655a2
Glenn Law <Glennlaw@me.com>
2020-01-04 22:26:55 -0500

    My very first start up workspace. eslint-airbnb, prettier, gulp, babel, etc

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb/recommended', 'prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
+    'comma-dangle': 'always-multiline',
+  },
+};
---
+++ app/js/main.js
@@ -0,0 +1,1 @@
+var jamRock = 10;
